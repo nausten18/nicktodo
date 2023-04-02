bdd797cb53f75dcdd43506ef219e2d21c953b2bc
Nicholas <nausten18@gmail.com>
2023-04-02 15:27:53 +1000

    Added navbar and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { AboutComponent } from './about/about.component';
+import { GuidesComponent } from './guides/guides.component';
+import { HomeComponent } from './home/home.component';
+import { SupportComponent } from './support/support.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    AboutComponent,
+    GuidesComponent,
+    HomeComponent,
+    SupportComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
